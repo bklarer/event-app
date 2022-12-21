02743841c5e9f0c8a20f43f6ba59dae40b3ffca2
Benjamin Klarer <benjaminklarer@gmail.com>
2022-12-20 21:07:27 -0700

    Adjust Links

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,14 +1,14 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { NavLink } from "react-router-dom";
+import {LinkContainer} from "react-router-bootstrap";
 
 // Will need state that shows if a user is logged in, which will change what shows in Navigation
 // May want to change NavBar
 
 function Navigation () {
 
-
+    // <NavLink style={{textDecoration: "none", color: "white", padding: 5}} to="/login">Login</NavLink>
 
     return (
 
@@ -16,12 +16,16 @@ function Navigation () {
             <Container>
                 <Navbar.Brand>Event App</Navbar.Brand>
                 <Nav className="d-flex">
-                    <Nav.Link>
-                        <NavLink style={{textDecoration: "none", color: "white"}}  to="/signup">Signup</NavLink>
-                    </Nav.Link>
-                    <Nav.Link>
-                        <NavLink style={{textDecoration: "none", color: "white"}} to="/login">Login</NavLink>
-                    </Nav.Link>
+                    <Nav.Item>
+                        <LinkContainer style={{textDecoration: "none"}} to="/signup">
+                            <Nav.Link style={{padding: 5}}>Signup</Nav.Link>
+                        </LinkContainer>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <LinkContainer style={{textDecoration: "none"}} to="/login">
+                            <Nav.Link style={{padding: 5}}>Login</Nav.Link>
+                        </LinkContainer>
+                    </Nav.Item>
                 </Nav>
             </Container>
 
