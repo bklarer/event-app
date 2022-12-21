514ac07452f9b22b184e5364e08fd4b013569a22
Benjamin Klarer <benjaminklarer@gmail.com>
2022-12-20 20:40:43 -0700

    Start EditEvent component

--- client/src/components/EditEvent.js
+++ client/src/components/EditEvent.js
@@ -1,5 +1,5 @@
-
-
+import { useState } from "react"
+import Form from 'react-bootstrap/Form';
 
 
 
@@ -16,7 +16,18 @@ function EditEvent () {
 
 
         <div>
+            <Form>
+                <Form.Group className="mb-3">
+                    <Form.Label>Event Title</Form.Label>
+                    <Form.Control type="text" placeholder="Event Title" />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>Event Description</Form.Label> 
+                    <Form.Control type="text" placeholder="Description" />
+                </Form.Group>
 
+
+            </Form>
 
 
 
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,5 +1,5 @@
 import Form from 'react-bootstrap/Form';
-
+import { useState } from "react"
 
 
 function Signup () {
