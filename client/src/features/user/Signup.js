71353081b5893a28e7f2d25bd61a423bba5981b8
Benjamin Klarer <benjaminklarer@gmail.com>
2022-12-21 20:31:38 -0700

    Make the Signup component a controlled form

--- client/src/features/user/Signup.js
+++ client/src/features/user/Signup.js
@@ -5,8 +5,22 @@ import { useState } from "react"
 
 function Signup () {
 
+    const [signup, setSignup] = useState({
+        username: "",
+        first_name: "",
+        last_name: "",
+        password: "",
+        confirm_password: ""
+    })
 
 
+
+    function handleChange(e) {
+        const {name, value} = e.target
+        setSignup((signup) => ({...signup, [name]: value}))
+    }
+
+    console.log(signup)
 
     return (
 
@@ -14,23 +28,53 @@ function Signup () {
             <Form >
                 <Form.Group className="mb-3">
                     <Form.Label>Username</Form.Label>
-                    <Form.Control type="text" placeholder="Username" />
+                    <Form.Control 
+                        type="text" 
+                        placeholder="Username"
+                        name="username"
+                        onChange={handleChange}
+                        value={signup.username}
+                    />
                 </Form.Group>
                 <Form.Group className="mb-3">
                     <Form.Label>First Name</Form.Label>
-                    <Form.Control type="text" placeholder="First Name" />
+                    <Form.Control 
+                        type="text" 
+                        placeholder="First Name" 
+                        name="first_name"
+                        onChange={handleChange}
+                        value={signup.first_name}
+                    />
                 </Form.Group>
                 <Form.Group className="mb-3">
                     <Form.Label>Last Name</Form.Label>
-                    <Form.Control type="text" placeholder="Last Name" />
+                    <Form.Control 
+                        type="text" 
+                        placeholder="Last Name" 
+                        name="last_name"
+                        onChange={handleChange}
+                        value={signup.last_name}
+                    />
                 </Form.Group>
                 <Form.Group className="mb-3">
                     <Form.Label>Password</Form.Label> 
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control 
+                        type="password" 
+                        placeholder="Password"
+                        name="password"
+                        onChange={handleChange}
+                        value={signup.password}
+                    />
                 </Form.Group>
                 <Form.Group className="mb-3">
                     <Form.Label>Confirm Password</Form.Label> 
-                    <Form.Control type="password" placeholder="Confirm Password" />
+                    <Form.Control 
+                        type="password" 
+                        placeholder="Confirm Password"
+                        name="confirm_password"
+                        onChange={handleChange}
+                        value={signup.confirm_password}
+                    />
                 </Form.Group>
                 <Button variant="primary" type="submit">
                     Submit
