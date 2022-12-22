89915a47623c231a26e3a2634899a5edac244977
Benjamin Klarer <benjaminklarer@gmail.com>
2022-12-21 20:19:33 -0700

    Make the Login component a controlled form

--- client/src/features/user/Login.js
+++ client/src/features/user/Login.js
@@ -7,7 +7,16 @@ import { useState } from "react"
 
 function Login () {
 
+    const [login, setLogin] = useState({
+        username: "",
+        password: ""
 
+    })
+
+    function handleChange(e) {
+        const {name, value} = e.target
+        setLogin((login) => ({...login, [name]: value}))
+    }
 
 
     return (
@@ -16,11 +25,23 @@ function Login () {
             <Form>
                 <Form.Group className="mb-3">
                     <Form.Label>Username</Form.Label>
-                    <Form.Control type="text" placeholder="Username" />
+                    <Form.Control 
+                        type="text" 
+                        placeholder="Username"
+                        name="username"
+                        onChange={handleChange}
+                        value={login.username}
+                    />
                 </Form.Group>
                 <Form.Group className="mb-3">
                     <Form.Label>Password</Form.Label> 
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control 
+                        type="password" 
+                        placeholder="Password"
+                        name="password"
+                        onChange={handleChange}
+                        value={login.password}
+                    />
                 </Form.Group>
                 <Button variant="primary" type="submit">
                     Submit
