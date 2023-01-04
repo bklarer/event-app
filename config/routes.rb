f5ba60cf9b9d3991412212a906d620552716d1f4
Benjamin Klarer <benjaminklarer@gmail.com>
2023-01-04 14:37:32 -0700

    Create routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   namespace :api do
     resources :tickets
     resources :events
-    resources :users
+    post "/signup", to: "users#create"
+    get "/me", to: "users#show"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
   end
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
