005be95b14e0829395d82bb07cee7f3c769b1e04
Benjamin Klarer <benjaminklarer@gmail.com>
2023-01-11 21:44:56 -0700

    Add validations to events

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,6 @@ class Event < ApplicationRecord
     has_many :tickets
     has_many :users, through: :tickets
 
+    validates :date :title, presence: true
     belongs_to :creator, class_name: "User"
 end
