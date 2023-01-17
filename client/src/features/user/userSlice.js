d8f7a806e90d3c23b19e9e27d35ad0e3241f79f4
Benjamin Klarer <benjaminklarer@gmail.com>
2023-01-16 19:04:04 -0700

    Adjust formatting

--- client/src/features/user/userSlice.js
+++ client/src/features/user/userSlice.js
@@ -1,5 +1,64 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { registerUser, userLogin, checkLogin } from "./userActions";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+export const registerUser = createAsyncThunk(
+    `user/signup`,
+    async (data, { rejectWithValue }) =>
+      await fetch("/api/signup", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(data),
+      }).then((response) => {
+        if (response.ok) {
+          return response.json().then((user) => {
+            return user;
+          });
+        } else {
+          return response.json().then((error) => {
+            return rejectWithValue(error.errors);
+          });
+        }
+      })
+  );
+  
+  export const userLogin = createAsyncThunk(
+    `user/login`,
+    async (data, { rejectWithValue }) =>
+      await fetch("/api/login", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(data),
+      }).then((response) => {
+        if (response.ok) {
+          return response.json().then((user) => {
+            return user;
+          });
+        } else {
+          return response.json().then((error) => {
+            return rejectWithValue(error.errors);
+          });
+        }
+      })
+  );
+  
+  export const checkLogin = createAsyncThunk(
+    `user/check`,
+    async () =>
+      await fetch("/api/me").then((response) => {
+        if (response.ok) {
+          return response.json().then((user) => {
+            return user;
+          });
+        }
+      })
+  );
+
 
 const initialState = {
   loading: false,
