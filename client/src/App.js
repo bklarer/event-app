7da3497287c0163afdc26e3199e113a8735b3ebd
Benjamin Klarer <benjaminklarer@gmail.com>
2023-01-16 18:00:59 -0700

    Move routes to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,37 @@
-// import "./styles/App.css";
 import NavagationBar from "./features/navigation/NavigationBar";
-import Layout from "./features/navigation/Layout";
 import Loading from "./features/navigation/Loading";
 import { useSelector, useDispatch } from "react-redux";
 import { checkLogin } from "./features/user/userActions";
 import { useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Sidebar from "./features/navigation/Sidebar";
+import Signup from "./features/user/Signup";
+import CreateEvent from "./features/events/CreateEvent";
+import { Route, Routes } from "react-router-dom";
+import Event from "./features/events/Event";
+import Home from "./features/navigation/Home";
+import EditEvent from "./features/events/EditEvent";
+import Account from "./features/user/Account";
+import HostedEvents from "./features/events/HostedEvents";
+import AttendingEvents from "./features/events/AttendingEvents";
+import AllEvents from "./features/events/AllEvents";
+import PageNotFound from "./features/navigation/PageNotFound";
 
 function App() {
-  const { loading } = useSelector((state) => state.user);
+  const { loading, userInfo } = useSelector((state) => state.user);
   const eventLoading = useSelector((state) => state.events.loading);
   const dispatch = useDispatch();
 
   useEffect(() => dispatch(checkLogin()), [dispatch]);
+
+  const sidebar = (
+    <Col md={3}>
+      {" "}
+      <Sidebar />{" "}
+    </Col>
+  );
+  const showSidebar = userInfo ? sidebar : null;
+
 
   return (
     <div className="App">
@@ -20,7 +40,31 @@ function App() {
       ) : (
         <>
           <NavagationBar />
-          <Layout />
+          <Container>
+      
+      <Row className="justify-content-md-center">
+        {showSidebar}
+
+        <Col md={5}>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/signup" element={<Signup />} />
+            <Route exact path="/events" element={<AllEvents />} />
+            <Route exact path="/events/new" element={<CreateEvent />} />
+            <Route exact path="/events/hosting" element={<HostedEvents />} />
+            <Route
+              exact
+              path="/events/attending"
+              element={<AttendingEvents />}
+            />
+            <Route exact path="/events/:eventId" element={<Event />} />
+            <Route exact path="/events/:eventId/edit" element={<EditEvent />} />
+            <Route exact path="/account" element={<Account />} />
+            <Route path="*" element={<PageNotFound/>}/>
+          </Routes>
+        </Col>
+      </Row>
+    </Container>
         </>
       )}
     </div>
